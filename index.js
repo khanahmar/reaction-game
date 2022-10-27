598ca9be8c7f2c455bb58de5bad93d1b97c33d68
Ahmar khan <ahmarbilal223344@gmail.com>
2022-10-27 18:27:58 +0500

    done

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-// on first click it should start
-// it should process
-// it should show the click
-// use should click
-
+// Getting id's
 const div = document.getElementById("div");
 const h1 = document.getElementById("h1");
+
+// Declearing variables
 let firstTime;
 let check = false;
 let check2 = false;
+let check3 = false;
 
+// Creating an event listner
 div.addEventListener("click", (e) => {
   if (div === e.target) {
     classProcess();
   }
   if (check === true) {
-    setTimeout(classClick, 3000);
+    setTimeout(classClick, 2000);
   }
   if (check2 === true && div === e.target) {
-    console.log(firstTime);
-    h1.innerHTML = Date.now()-firstTime;
+    h1.innerHTML = `Your reation time is :${Date.now() - firstTime}`;
   }
 });
 
+// Adding the class of click
 function classProcess() {
   div.classList.add("process");
   div.innerHTML = "in process....";
   check = true;
 }
 
+// Adding the class of click
 function classClick() {
   div.classList.add("click");
   div.innerHTML = "Now click";
@@ -36,6 +37,11 @@ function classClick() {
   check2 = true;
 }
 
+// Getting the reaction time
 function timeStart() {
   return (firstDate = Date.now());
 }
+
+// Getting time in seconds
+let newDateTime = Number(new Date(Date.now() - firstTime).getTime() / 1000);
+console.log(newDateTime);
